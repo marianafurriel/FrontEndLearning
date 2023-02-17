67daab8d13a018699c50224cbbab428330a72f6d
Mariana Furriel <marianafurrielss@gmail.com>
2023-02-17 18:02:53 -0300

    Projeto finalizado :)

--- interactive rating component main/script.js
+++ interactive rating component main/script.js
@@ -1,8 +1,22 @@
-const numbers = document.querySelectorAll('.number')
+const numbers = document.querySelectorAll('.number');
+const submit = document.querySelector('.enviar');
+const avaliacao = document.querySelector('.avaliacao');
+const thankyou = document.querySelector('.thankyou');
+const ratingSpan = document.querySelector('.rating')
+var rating = 0
 
 numbers.forEach(number=>number.addEventListener('click',toggleSelected))
+submit.addEventListener('click',enviar);
 
 function toggleSelected() {
     numbers.forEach(number => number.classList.remove('selected'));
     this.classList.toggle('selected');
+    rating = this.innerText;
+    console.log(rating)
+}
+
+function enviar(){
+    avaliacao.classList.add('hide');
+    thankyou.classList.remove('hide');
+    ratingSpan.innerText = rating;
 }
