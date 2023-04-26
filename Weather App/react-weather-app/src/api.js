8a3fbbd8ea7d367c740d9d2ce6661cc84e884f57
Mariana Furriel <marianafurrielss@gmail.com>
2023-04-26 05:26:37 -0300

    React Weather App adicionado

---
+++ Weather App/react-weather-app/src/api.js
@@ -0,0 +1,15 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '17e54a3bf6mshc9a2d84525fa366p15c5f3jsnfe48fdcfa270',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const openWeatherOptions={
+	method: 'GET',
+	headers:{
+		
+	}
+}
