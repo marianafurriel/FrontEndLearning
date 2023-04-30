bb3403dac145fb5c9ca6ee4a99b2d8a7ff175917
Mariana Furriel <marianafurrielss@gmail.com>
2023-04-30 13:23:54 -0300

    Já recebe corretamente os dados da api

--- breaking-bad-quotes/src/App.js
+++ breaking-bad-quotes/src/App.js
@@ -6,7 +6,7 @@ import Quote from './components/quote/quote';
 function App() {
   const [quote, setQuote] = useState(null);
   const [author, setAuthor] = useState(null);
-  const handleNewQuote = useEffect(() => {
+  const getQuote = () => {
     fetch(`${process.env.REACT_APP_BB_URL}/v1/quotes`)
       .then(response => response.json())
       .then(data => {
@@ -14,13 +14,16 @@ function App() {
         setAuthor(data[0].author);
       })
       .catch(error => console.error(error));
-  })
+  }
+  useEffect(() => {
+    getQuote();
+  }, [])
   console.log(quote,author)
   return (
     <div className='container'>
-      {/* <p>{quote.quote}</p> */}
+      <p>{quote}</p>
       {/* <Quote data={[quote,author]}/> */}
-      <button>Another quote</button>
+      <button onClick={getQuote}>Another quote</button>
 
     </div>
   );
