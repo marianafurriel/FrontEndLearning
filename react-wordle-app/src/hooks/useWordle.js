9862eb958e4189f5ce2061909c202960455e924e
Mariana Furriel <marianafurrielss@gmail.com>
2023-05-02 05:48:26 -0300

    Validação de tentativas finalizada

--- react-wordle-app/src/hooks/useWordle.js
+++ react-wordle-app/src/hooks/useWordle.js
@@ -5,19 +5,36 @@ const useWordle = (solution) => {
   const [turn, setTurn] = useState(0)
   const [currentGuess, setCurrentGuess] = useState('')
   const [guesses, setGuesses] = useState([]) // each guess is an array
-  const [history, setHistory] = useState([]) // each guess is a string
+  const [history, setHistory] = useState(['hello', 'ninja']) // each guess is a string
   const [isCorrect, setIsCorrect] = useState(false)
 
-  const formatGuess = () => { }
+  const formatGuess = () => {
+  console.log("formatting the guess: ",currentGuess)
+   }
 
   const addNewGuess = () => { }
 
   const handleKeyup = ({ key }) => {
-    console.log(key)
+    if (key === 'Enter') {
+      if (turn > 5) { //teste se ainda não esgotou as tentativas
+        console.log("You used all your guesses");
+        return
+      }
+      if (history.includes(currentGuess)) { //testa se a tentativa está repetida
+        console.log("you already tried that word")
+        return
+      }
+      if (currentGuess.length !== 5) {
+        console.log("your word must be 5 chars long")
+      }
+      
+      formatGuess()
+    }
 
-    if(key === 'Backspace'){
+    if (key === 'Backspace') {
       setCurrentGuess(prev => {
-        return prev.slice(0,-1)})
+        return prev.slice(0, -1)
+      })
     }
 
     if (/^[A-Za-z]$/.test(key)) {
