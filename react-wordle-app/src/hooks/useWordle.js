a7c2bb0dcdf42fb3190cf49097604bb3639b6c70
Mariana Furriel <marianafurrielss@gmail.com>
2023-05-02 05:28:52 -0300

    Letras já são registradas e apagadas quando o usuario digita

---
+++ react-wordle-app/src/hooks/useWordle.js
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+
+const useWordle = (solution) => {
+
+  const [turn, setTurn] = useState(0)
+  const [currentGuess, setCurrentGuess] = useState('')
+  const [guesses, setGuesses] = useState([]) // each guess is an array
+  const [history, setHistory] = useState([]) // each guess is a string
+  const [isCorrect, setIsCorrect] = useState(false)
+
+  const formatGuess = () => { }
+
+  const addNewGuess = () => { }
+
+  const handleKeyup = ({ key }) => {
+    console.log(key)
+
+    if(key === 'Backspace'){
+      setCurrentGuess(prev => {
+        return prev.slice(0,-1)})
+    }
+
+    if (/^[A-Za-z]$/.test(key)) {
+      if (currentGuess.length < 5) {
+        setCurrentGuess(prev => { return prev + key })
+      }
+    }
+  }
+
+  return { turn, currentGuess, guesses, isCorrect, handleKeyup }
+
+}
+export default useWordle
