469658a4b9295ff3c943e8124de8a42d818fc6e0
Mariana Furriel <marianafurrielss@gmail.com>
2023-05-09 20:48:03 -0300

    Agora pega palavras de uma API

--- react-wordle-app/src/App.js
+++ react-wordle-app/src/App.js
@@ -1,19 +1,17 @@
 import {useEffect, useState} from 'react';
 import Wordle from './components/wordle';
+import { ranWordsOptions } from './api';
 
 function App() {
   const [solution,setSolution] = useState(null);
 
   useEffect(()=>{
-    fetch('http://localhost:3001/solutions')
-    .then(res=>res.json())
-    .then(json =>{
-      //random int between 0 and 14
-      const randomSolution = json[Math.floor(Math.random()*json.length)]
-      setSolution(randomSolution.word)
+    fetch("https://random-words5.p.rapidapi.com/getRandom?wordLength=5",ranWordsOptions)
+    .then(res=>res.text())
+    .then(text =>{
+      setSolution(text)
     })
   },[setSolution])
-  console.log(solution)
   
   return (
     <div className="App">
---
+++ react-wordle-app/src/api.js
@@ -0,0 +1,7 @@
+export const ranWordsOptions = {
+	method: 'GET',
+	headers: {
+			'X-RapidAPI-Key': process.env.REACT_APP_WORDS_API_KEY,
+		'X-RapidAPI-Host': 'random-words5.p.rapidapi.com'
+	}
+};
