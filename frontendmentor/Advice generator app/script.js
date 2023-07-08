9efc4b65caff7006ba81e35830fd2a57d6d94085
Mariana Furriel <marianafurrielss@gmail.com>
2023-07-08 08:55:34 -0300

    Projeto finalizado :rocket:

--- frontendmentor/Advice generator app/script.js
+++ frontendmentor/Advice generator app/script.js
@@ -3,8 +3,18 @@ const botao = document.querySelector(".botao");
 const pQuote = document.querySelector(".advice");
 const adId = document.querySelector(".numAd");
 
-botao.addEventListener("click",novoQuote);
+botao.addEventListener("click", novoQuote);
 
-function novoQuote(){
-  const quote = fetch()
+function novoQuote() {
+  const response = fetch('https://api.adviceslip.com/advice').
+    then((res) => res.json())
+    .then((data) => {
+      setQuotes(data.slip);
+    })
+}
+
+function setQuotes(quoteObj) {
+  pQuote.textContent = quoteObj.advice;
+  adId.textContent = quoteObj.id;
+  console.log(quoteObj)
 }
