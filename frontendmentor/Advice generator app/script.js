1dca282f26bacd43afb531ed94b4d2d92ba7a7dd
Mariana Furriel <marianafurrielss@gmail.com>
2023-07-08 07:59:15 -0300

    script iniciado

---
+++ frontendmentor/Advice generator app/script.js
@@ -0,0 +1,10 @@
+novoQuote();
+const botao = document.querySelector(".botao");
+const pQuote = document.querySelector(".advice");
+const adId = document.querySelector(".numAd");
+
+botao.addEventListener("click",novoQuote);
+
+function novoQuote(){
+  const quote = fetch()
+}
